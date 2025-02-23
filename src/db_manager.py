b91a3883dc32d8e14d384b0502f0454d5db7c4f6
Alex Pitzer <alexpitzer@Shrutis-MacBook-Air.local>
2025-02-23 12:37:29 -0800

    m

--- src/db_manager.py
+++ src/db_manager.py
@@ -11,13 +11,13 @@ class DBManager:
     def insert(self, values):
         cursor = self.db.cursor()
         values = [None] + list(values)
-        prepared_statement = "INSERT INTO summarized (id, summary, file_path) VALUES (?, ?, ?)"
+        prepared_statement = "INSERT INTO summarized (id, summary, file_path) VALUES (?, ?, ?) ON CONFLICT(file_path) DO UPDATE SET summary=excluded.summary"
         insert = cursor.execute(prepared_statement, values)
         self.db.commit()
         return insert
 
     def global_init(self):
-        query = "CREATE TABLE IF NOT EXISTS summarized (id INTEGER PRIMARY KEY, summary TEXT, file_path TEXT)"
+        query = "CREATE TABLE IF NOT EXISTS summarized (id INTEGER PRIMARY KEY, summary TEXT, file_path TEXT UNIQUE)"
         cursor = self.db.cursor()
         cursor.execute(query)
         return cursor.fetchall()
